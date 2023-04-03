41329a7167058d8e181b6eeb17f89ac5f3bb403e
Juan José Aranzales Ochoa <juanjoaranzales@gmail.com>
2023-04-03 13:54:15 -0500

    Add general characters

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_characters/utils/extensions.dart';
+import 'package:marvel_characters/widgets/list_row_tile_characters.dart';
 import 'package:marvel_characters/widgets/list_tile_character.dart';
 
 import '../widgets/custom_padding.dart';
@@ -76,7 +77,59 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-            )
+            ),
+            30.pv,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Other characters may include:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            15.pv,
+            Expanded(
+              child: ListView(padding: EdgeInsets.zero, children: [
+                const ListTileRowCharacters(
+                  title: 'Iron Man',
+                  image: 'assets/IronMan.jpg',
+                  events: 4,
+                  comics: 30,
+                  series: 12,
+                ),
+                15.pv,
+                const ListTileRowCharacters(
+                  title: 'Spiderman',
+                  image: 'assets/spiderman.jpg',
+                  events: 3,
+                  comics: 40,
+                  series: 32,
+                ),
+                15.pv,
+                const ListTileRowCharacters(
+                  title: 'Thor',
+                  image: 'assets/thor.jpg',
+                  events: 1,
+                  comics: 10,
+                  series: 2,
+                ),
+                15.pv,
+                const ListTileRowCharacters(
+                  title: 'Captain America',
+                  image: 'assets/captain-america.png',
+                  events: 0,
+                  comics: 7,
+                  series: 1,
+                ),
+              ]),
+            ),
           ],
         ),
       ),
